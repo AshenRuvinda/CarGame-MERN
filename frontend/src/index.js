72b5b8257b451b6f89927a064248f40513cc7743
Ashen Ruvinda <138576609+AshenRuvinda@users.noreply.github.com>
2025-08-29 13:20:36 +0530

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'; // This line imports Tailwind CSS
 import App from './App';
-import './styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
